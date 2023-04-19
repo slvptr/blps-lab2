39e01f08e1faf07c9b00fae4f3a1ece3654cd091
callmepedro <callmepedro@mail.ru>
2023-04-19 17:19:35 +0300

    Fix rate

--- src/main/java/blps/lab2/controller/topic/RateController.java
+++ src/main/java/blps/lab2/controller/topic/RateController.java
@@ -1,14 +1,13 @@
 package blps.lab2.controller.topic;
 
 import blps.lab2.model.domain.topic.Topic;
+import blps.lab2.model.domain.user.User;
 import blps.lab2.model.responses.topic.TopicView;
 import blps.lab2.service.rate.RateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
@@ -21,12 +20,12 @@ public class RateController {
         this.rateService = rateService;
     }
 
-    @PostMapping("/")
+    @PostMapping("/{id}")
     public TopicView rateTopic(
-            @RequestParam(value = "userId", defaultValue = "", required = false) String userId,
-            @RequestParam(value = "topicId",  defaultValue = "", required = false) String topicId) {
+            @PathVariable String id) {
         try {
-            Topic topic = rateService.rateTopic(Long.parseLong(userId), Long.parseLong(topicId));
+            User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            Topic topic = rateService.rateTopic(user.getId(), Long.parseLong(id));
             return TopicView.fromTopic(topic);
         } catch (NumberFormatException e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
